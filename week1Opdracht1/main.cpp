1d9c23aa8dbcab971abc24a628d127d2a9fd4eef
paulien <paulien@medusa-online.com>
2022-09-07 13:15:41 +0200

    add code for first lesson

--- week1Opdracht1/main.cpp
+++ week1Opdracht1/main.cpp
@@ -6,7 +6,26 @@
 
 int main(void) {
 
-	printf("Hello World!");
+	int a;
+	int b; 
+	int c;
+
+	a = 10;
+	b = 20;
+
+	printf("Voer een heel getal in..");
+	scanf_s("%d", &b);
+
+	c = a * b;
+
+	if (b > 9) {
+
+		printf("het ingevoerde getal is groter dan 9\n");
+
+	}
+	else {}
+	printf("Programmeren is %d leuk! %d \n" , a, b);
+	printf("A keer B is: %d", c);
 
 	return 0;
 }
